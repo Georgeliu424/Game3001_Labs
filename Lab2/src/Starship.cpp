6d439808aac2ebc252a580bbf384f4f930e6c2a4
Georgeliu424 <34744507+Georgeliu424@users.noreply.github.com>
2023-01-24 15:46:16 -0500

    configured changes to starship in play scene

--- Lab2/src/Starship.cpp
+++ Lab2/src/Starship.cpp
@@ -7,11 +7,19 @@ Starship::Starship()
 	const auto size = TextureManager::Instance().GetTextureSize("starship");
 	SetWidth(static_cast<int>(size.x));
 	SetHeight(static_cast<int>(size.y));
-	GetTransform()->position = glm::vec2(100.0f, 100.0f);
+	GetTransform()->position = glm::vec2(0.0f, 0.0f);
+	GetRigidBody()->bounds = glm::vec2(GetWidth(),GetHeight());
 	GetRigidBody()->velocity = glm::vec2(0, 0);
 	GetRigidBody()->isColliding = false;
 
 	SetType(GameObjectType::AGENT);
+
+	m_maxSpeed = 50.0f;//a max number of pixels moved per frames
+	m_turnRate = 5.0f; //a max degree of pixels to turn 
+	m_accelerationRate = 4.0f;//a max number of pixels to add to the velocity each frame
+
+	SetCurrentDirection((glm::vec2(1.0f, 0.0f))); //facing right 
+
 }
 
 Starship::~Starship() = default;
@@ -19,6 +27,7 @@ Starship::~Starship() = default;
 void Starship::Draw()
 {
 	TextureManager::Instance().Draw("starship", GetTransform()->position, 0, 255, true);
+	GetTransform()->position, static_cast<double>(GetCurrentHeading(), 255, true);
 }
 
 void Starship::Update()
@@ -26,5 +35,56 @@ void Starship::Update()
 }
 
 void Starship::Clean()
+{
+}
+
+float Starship::GetMaxSpeed() const
+{
+	return m_maxSpeed;
+}
+
+float Starship::GetTurnRate() const
+{
+	return m_turnRate;
+}
+
+float Starship::GetAccelerationRate() const
+{
+	return m_accelerationRate;
+}
+
+glm::vec2 Starship::GetDesiredVelocity() const
+{
+	return m_desiredVelocity;
+}
+
+void Starship::SetMaxSoeed(float Speed)
+{
+	m_maxSpeed = Speed;
+}
+
+void Starship::SetTurnRate(float Angle)
+{
+	m_turnRate = Angle;
+}
+
+void Starship::SetAccelerationRate(float rate)
+{
+	m_accelerationRate = rate;
+}
+
+void Starship::SetDesiredVelocity(glm::vec2 target_postion)
+{
+}
+
+void Starship::seek()
+{
+}
+
+void Starship::Lookwhereyouregoing(glm::vec2 target_direction)
+{
+}
+
+void Starship::m_move()
 {
 }
--- Lab2/src/Starship.h
+++ Lab2/src/Starship.h
@@ -17,10 +17,31 @@ public:
 	void Update() override;
 	void Clean() override;
 
+	//getters and setters
+	float GetMaxSpeed() const;
+	float GetTurnRate() const;
+	float GetAccelerationRate() const;
+	glm::vec2 GetDesiredVelocity() const;
+
+	void SetMaxSoeed(float Speed);
+	void SetTurnRate(float Angle);
+	void SetAccelerationRate(float rate);
+	void SetDesiredVelocity(glm::vec2 target_postion);
+
+	void seek();
+	void Lookwhereyouregoing(glm::vec2 target_direction);
+
 
 private:
+	//private movement variable
+	float m_maxSpeed;
+	float m_turnRate;
+	float m_accelerationRate;
 
 
+	glm::vec2 m_desiredVelocity;
+
+	void m_move();
 
 
 
