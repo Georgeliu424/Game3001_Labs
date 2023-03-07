affee43d984246627a8d1ae09eba446297f93a71
Georgeliu424 <34744507+Georgeliu424@users.noreply.github.com>
2023-03-07 17:54:36 -0500

    cleaned up Play Scene

--- Lab6.1/src/PlayScene.h
+++ Lab6.1/src/PlayScene.h
@@ -5,11 +5,7 @@
 #include "Scene.h"
 #include "Target.h"
 #include "StarShip.h"
-#include "Tile.h"
-#include "Heuristic.h"
 #include "Obstacle.h"
-#include <queue>
-#include "TileComparator.h"
 
 class PlayScene : public Scene
 {
@@ -36,63 +32,14 @@ private:
 	// Debugging Variables
 	bool m_bDebugView;
 
-	// Pathfinding Objects and Functions
-	std::vector<Tile*> m_pGrid;
 	bool m_isGridEnabled;
 
 	void m_buildGrid();
-	bool m_getGridEnabled() const;
-	void m_setGridEnabled(bool state);
-	void m_computeTileCosts();
 	
 	// obstacle list
 	std::vector<Obstacle*> m_pObstacles;
 	void m_buildObstacles();
 
-	//obstacle utility functions
-	void m_removeObstacleAt(int col, int row);
-	void m_removeObstacleAt(glm::vec2 grid_position);
-	void m_removeAllObstacles();
-
-	// collections,variables for pathfinding
-	std::priority_queue<Tile*,std::vector<Tile*>,TileComparator> m_pOpenList;// visited but not expanded list of tiles
-	std::list<Tile*>m_pPathList;
-	bool m_pathFound = false;
-
-	// pathfinding functions 
-	void m_findShortestPath();
-	void m_buildPathList();
-	void m_displayPathList() const;
-	void m_resetPathFinding();
-	void m_resetSimulation();
-
-
-	// convenience Functions to convert world to grid space
-	Tile* m_getTile(int col, int row) const;
-	Tile* m_getTile(glm::vec2 grid_position) const;
-
-	template <typename T>
-	void m_addObjectToGrid(T*& object, int col, int row, TileStatus status);
-
-	template<typename T>
-	void m_moveGameObject(T*& object, int col, int row, TileStatus status);
-
-	// heuristic
-	Heuristic m_currentHeuristic;
-
-	//Tile map variables and functions
-	std::string m_tileMap;
-	void m_initializeTileMap();
-	void m_buildTileMap();
-
-	// convenience functions to update the Tile Map
-	void m_updateTileMap(int col, int row, TileStatus status);
-	void m_updateTileMap(glm::vec2 grid_position, TileStatus status);
-
-	//Ship moving functions 
-	//int m_moveCounter = 0;
-	//bool m_shipIsMoving = false;
-	//void m_moveStarship();
 
 };
 
